9d2e913543addc7f7486e86da8c725f5aa2f8ec8
Mahesh Paul <123456magimagi@gmail.com>
2025-10-16 09:13:14 +0530

    devops,

--- lib/edgestoreProvider.ts
+++ lib/edgestoreProvider.ts
@@ -1,9 +1,7 @@
-'use client';
- 
-import { type EdgeStoreRouter } from './edgestore';
+import { type EdgeStoreRouter } from './edgestore'; // This type-only import is safe
 import { createEdgeStoreProvider } from '@edgestore/react';
- 
+ 
 const { EdgeStoreProvider, useEdgeStore } =
   createEdgeStoreProvider<EdgeStoreRouter>();
- 
+ 
 export { EdgeStoreProvider, useEdgeStore };
