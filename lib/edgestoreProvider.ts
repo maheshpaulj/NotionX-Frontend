7466420c73573c96c638651e9f27b967914318e6
Mahesh Paul <123456magimagi@gmail.com>
2025-10-16 08:59:39 +0530

    devops finally

--- lib/edgestoreProvider.ts
+++ lib/edgestoreProvider.ts
@@ -1,10 +1,9 @@
 'use client';
-
-import { type EdgeStoreRouter } from './edgestore'; // Import the TYPE from the server file
+ 
+import { type EdgeStoreRouter } from './edgestore';
 import { createEdgeStoreProvider } from '@edgestore/react';
- 
+ 
 const { EdgeStoreProvider, useEdgeStore } =
   createEdgeStoreProvider<EdgeStoreRouter>();
- 
-// We export the client-side provider and hook from this file.
+ 
 export { EdgeStoreProvider, useEdgeStore };
