6ade9a758a79cb75b646e1e55967c59ab8d3942b
Mahesh Paul <123456magimagi@gmail.com>
2025-10-22 16:38:59 +0530

    DEvOPs W-W

--- components/Providers/LiveBlocksProvider.tsx
+++ components/Providers/LiveBlocksProvider.tsx
@@ -1,16 +1,15 @@
 "use client";
 
 import { LiveblocksProvider } from '@liveblocks/react/suspense'
-import { env } from 'process';
 
 function LiveBlocksProvider({children}:{children: React.ReactNode}) {
 
-    if(!env.NEXT_PUBLIC_LIVEBLOCKS_PUBLIC_KEY){
-        throw new Error("public liveblocks key not found")
-    }
+    // if(process.env.NEXT_PUBLIC_LIVEBLOCKS_PUBLIC_KEY){
+    //     throw new Error("public liveblocks key not found")
+    // }
 
   return (
-    <LiveblocksProvider authEndpoint={"/api/auth-endpoint"} throttle={16} publicApiKey={env.NEXT_PUBLIC_LIVEBLOCKS_PUBLIC_KEY}>
+    <LiveblocksProvider authEndpoint={"/api/auth-endpoint"} throttle={16}>
         {children}
     </LiveblocksProvider>
   )
